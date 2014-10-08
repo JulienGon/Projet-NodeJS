d5ada52c1bd9602c6e679012d215525396d44336
JulienGon <julien.guyon-travers@etu-webschoolfactory.fr>
2014-10-09 01:07:37 +0200

    Projet Node JS version finale
    Affichage côté client de la variable calcul.

--- public/script.js
+++ public/script.js
@@ -5,17 +5,26 @@
 
 	var ulTweets = document.getElementById('tweets');
 
-	socket.on('tweet', function(tweet) {
-		var firstLi = ulTweets.firstChild;
+	// socket.on('tweet', function(tweet) {
+	// 	var firstLi = ulTweets.firstChild;
 
-		var li = document.createElement('li');
-		li.innerHTML = tweet.text;
+	// 	var li = document.createElement('li');
+	// 	li.innerHTML = tweet.text;
 
-		if (!firstLi) {
-			ulTweets.appendChild(li);
-		}
-		else {
-			ulTweets.insertBefore(li, firstLi);
-		}
+	// 	if (!firstLi) {
+	// 		ulTweets.appendChild(li);
+	// 	}
+	// 	else {
+	// 		ulTweets.insertBefore(li, firstLi);
+	// 	}
+	// });
+
+
+	socket.on('StatUpdate', function(calcul, tag) {
+		console.log('calcul' + calcul);
+		console.log('tag' + tag);
+		$('.Ticket').empty();
+		$('.Ticket').append('<h1>' + tag + '</h1> <h2>' + calcul + '</h2>')
 	});
+
 })();
